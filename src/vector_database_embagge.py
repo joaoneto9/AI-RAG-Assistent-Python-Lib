5b8816b9922eacefd8d07732542d877e72abca1f
joaoneto9 <joaonetocg1@gmail.com>
2026-04-01 02:09:31 -0300

    need-fix: after testing the database i looked foward that the split-line of a .md archive is a bad idea

--- src/vector_database_embagge.py
+++ src/vector_database_embagge.py
@@ -1,11 +1,47 @@
 from chromadb import PersistentClient
+import os
+import uuid
+
+def get_collection():
+    return collection
 
 # Create the vect database
-
 client = PersistentClient(path="./vectorial_database")
-
 collection = client.get_or_create_collection(name="python-lib-collection")
 
-print("creating or geting the vectorial database")
+if __name__ == "__main__":
+    print("creating or geting the vectorial database")
 
 # Populate the Vect Database
+for archive in os.listdir("docs/langgraph"):
+    with open(f"docs/langgraph/{archive}", "r", encoding="utf-8") as f:
+        content: list[str] = f.read().splitlines() # pega cada linha 
+    
+    collection.add(
+        ids=[str(uuid.uuid4()) for _ in content],
+        documents=content,
+        metadatas=[{
+            "line": line,
+            "archive_content": archive
+        } for line in range(len(content))]
+    )
+
+if __name__ == "__main__":
+    print("database vector was build or alredy exists")
+    print(collection.peek())
+
+    print("test if the database is correpondenting:")
+
+    results = collection.query(
+        query_texts=[
+            "How can i install the langgraph lib?",
+            "where is the local server?"
+        ],
+        n_results=5,
+    )
+
+    for i, r in enumerate(results["documents"]):
+        print(f"\nquery {i}:")
+        print("\n".join(r))
+
+
